4e71feb3cdb8f05dcd61a4702defcf49256e6fb0
André Donadon <andre.donadon@outlook.com>
2021-04-20 17:08:05 -0300

    Corecting do method on class client_core and implementing commom headers with singleton pattern

--- gohttp/client_core.go
+++ gohttp/client_core.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func do(method string, url string, headers http.Header, body interface{}) (*http.Response, error) {
+func (c *httpClient) do(method string, url string, headers http.Header, body interface{}) (*http.Response, error) {
 	client := http.Client{}
 
 	request, err := http.NewRequest(method, url, nil)
@@ -14,5 +14,26 @@ func do(method string, url string, headers http.Header, body interface{}) (*http
 		return nil, errors.New("unable to create a new request")
 	}
 
+	fullHeaders := c.getRequestHeaders(headers)
+	request.Header = fullHeaders
+
 	return client.Do(request)
+}
+
+func (c *httpClient) getRequestHeaders(requestHeaders http.Header) http.Header {
+	result := make(http.Header)
+	// Add common headers to the request:
+	for header, value := range c.Headers {
+		if len(value) > 0 {
+			result.Set(header, value[0])
+		}
+	}
+
+	// Add custom headers to the request:
+	for header, value := range requestHeaders {
+		if len(value) > 0 {
+			result.Set(header, value[0])
+		}
+	}
+	return result
 }
--- main.go
+++ main.go
@@ -7,15 +7,35 @@ import (
 	"github.com/donadonandre/go-httpclient/gohttp"
 )
 
-func main() {
+var (
+	// Singleton here
+	gitHubHttpClient = getGitHubClient()
+)
+
+func getGitHubClient() gohttp.HttpClient {
 	client := gohttp.New()
 
-	response, err := client.Get("https://api.github.com", nil)
+	/*commonHeaders := make(http.Header)
+	commonHeaders.Set("Authorization", "Bearer ABC-123")
+
+	client.SetHeaders(commonHeaders)*/
+	return client
+}
+
+func main() {
+	getUrls()
+}
+
+func getUrls() {
+	//headers := make(http.Header)
+	// headers.Set("Authorization", "Bearer ABC-123")
+
+	response, err := gitHubHttpClient.Get("https://api.github.com", nil)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.PrintLn(response.StatusCode)
+	fmt.Println(response.StatusCode)
 
 	bytes, _ := ioutil.ReadAll(response.Body)
 	fmt.Println(string(bytes))
