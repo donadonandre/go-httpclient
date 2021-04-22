9bdf7e81fbe9043cfb780bc0fe32b972f48054e5
André Donadon <andre.donadon@outlook.com>
2021-04-22 15:51:06 -0300

    Class 15 - Testing many scenarions in unique test func

--- gohttp/client_core_test.go
+++ gohttp/client_core_test.go
@@ -1,6 +1,7 @@
 package gohttp
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 )
@@ -35,4 +36,69 @@ func TestGetRequestHeaders(t *testing.T) {
 	if finalHeaders.Get("X-Request-Id") != "ABC-123" {
 		t.Error("Invalid request id received")
 	}
+}
+
+func TestGetRequestBodyNilBody(t *testing.T) {
+	// Initialization
+	client := httpClient{}
+
+	t.Run("noBodyNilResponse", func(t *testing.T) {
+		// Execution
+		body, err := client.getRequestBody("", nil)
+
+		// Validation
+		if err != nil {
+			t.Error("No error expecting when passing a nil body")
+		}
+
+		if body != nil {
+			t.Error("No body expecting when passing a nil body")
+		}
+	})
+
+	t.Run("BodyWithJson", func(t *testing.T) {
+		// Execution
+		requestBody := []string{"one", "two"}
+		body, err := client.getRequestBody("application/json", requestBody)
+
+		// Validation
+		if err != nil {
+			t.Error("No error expecting when marshaling slice as json")
+		}
+
+		if string(body) != `["one","two"]` {
+			t.Error("Invalid json body obtained")
+		}
+	})
+
+	t.Run("BodyWithXml", func(t *testing.T) {
+		// Execution
+		requestBody := []string{"one", "two"}
+		body, err := client.getRequestBody("application/xml", requestBody)
+
+		// Validation
+		if err != nil {
+			t.Error("No error expecting when marshaling slice as xml")
+		}
+
+		if string(body) != `<string>one</string><string>two</string>` {
+			fmt.Println("body:", string(body))
+			t.Error("Invalid xml body obtained")
+		}
+	})
+
+	t.Run("BodyWithJsonAsDefault", func(t *testing.T) {
+		// Execution
+		requestBody := []string{"one", "two"}
+		body, err := client.getRequestBody("", requestBody)
+
+		// Validation
+		if err != nil {
+			t.Error("No error expecting when marshaling slice as json as default")
+		}
+
+		if string(body) != `["one","two"]` {
+			t.Error("Invalid json body obtained")
+		}
+	})
 }
